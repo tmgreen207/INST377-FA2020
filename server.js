3083539d1fcb3d23980d91fbb2a19f0d846ee9b5
TaylorGreen207 <tmgreen207@gmail.com>
2020-11-04 13:04:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
-import countries from './public/lab_6/countries.js'
+import countries from './public/lab_6/countries.js';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
